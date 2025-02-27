93dc9882c2cc5f1f9f9e4bcd446fedd3f2fa16a7
Chineze-prog <chinezeobi.bomi@gmail.com>
2025-02-27 17:13:48 -0600

    added unit tests for client side of leaderboard feature

--- client/src/__tests__/Leaderboard.test.js
+++ client/src/__tests__/Leaderboard.test.js
@@ -4,8 +4,9 @@ import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Leaderboard from '../components/Leaderboard';
 
+
 describe('Leaderboard Component', () => {
-  test('renders Leaderboard component', () => {
+  test('renders sorting dropdown options', () => {
     render(
       <Router>
         <Leaderboard />
@@ -13,6 +14,21 @@ describe('Leaderboard Component', () => {
     );
 
     // Add assertions to verify the rendered output
+    expect(screen.getByText('Username')).toBeInTheDocument();
     expect(screen.getByText('Wins')).toBeInTheDocument();
+    expect(screen.getByText('Losses')).toBeInTheDocument();
+    expect(screen.getByText('Win/Loss Ratio')).toBeInTheDocument();
+    expect(screen.getByText('Money Spent')).toBeInTheDocument();
+    expect(screen.getByText('Time Spent')).toBeInTheDocument();
+  });
+
+  test('displays "No data available" when leaderboard is empty', () => {
+    render(
+      <Router>
+        <Leaderboard />
+      </Router>
+    );
+
+    expect(screen.getByText('No data available')).toBeInTheDocument();
   });
 });
