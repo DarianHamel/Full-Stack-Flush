7d38e2084f0dc7746fb8b748182e4bf1c26b45f5
Chineze-prog <chinezeobi.bomi@gmail.com>
2025-02-26 19:51:18 -0600

    splitting leaderboard logic for unit testing

--- server/Controllers/LeaderboardController.js
+++ server/Controllers/LeaderboardController.js
@@ -11,37 +11,14 @@ module.exports.GetLeaderboard = async(req, res) => {
             return res.status(400).json({error: "Invalid sorting option"});
         }
 
-       // let collection = await db.collection("leaderboard");
+        let filterCriteria = module.exports.applyFilters(filter);
 
-        // filtering options
-        let filterCriteria = {};
-        if(filter === "highSpenders"){
-            filterCriteria = {moneySpent: {$gte: 1500}} //users that have spent exactly or over $1500 are considered high spenders
-        }
-        else if(filter === "longestPlayers"){
-            filterCriteria = {timeSpent: {$gte: 100}} //users that have spent exactly or over 100hrs are considered the longest players
-        }
-
-        let results = await User.find();
+        let results = await User.find(filterCriteria);
         console.log(results);
-        //res.status(200).json(results);
+
         results = JSON.parse(JSON.stringify(results));
-
-        //im not sure if the win/loss ratio would be part of the db but for now just calculate it based on the wins and losses
-        //add the calculated win/loss ratio to the results sent
-        results = results.map(user => ({
-            ...user,
-            winLossRatio: user.losses > 0 ? (user.wins/user.losses).toFixed(2): user.wins
-        }));
-
-        //sort the results based on the selected option
-        results.sort((user1, user2) => {
-            if(sortBy === "username"){
-                return order === "asc" ? user1.username.toLowerCase().localeCompare(user2.username.toLowerCase()) : user2.username.toLowerCase().localeCompare(user1.username.toLowerCase());
-            }
-
-            return order === "asc" ? user1[sortBy] - user2[sortBy] : user2[sortBy] - user1[sortBy];
-        });
+        results = module.exports.calculateWinLossRatio(results);
+        results = module.exports.sortLeaderboard(results, sortBy, order);
 
         console.log(`Leaderboard data sorted by ${sortBy}, in order ${order} with filter ${filter}: `, results);
         res.status(200).json(results);
@@ -51,3 +28,42 @@ module.exports.GetLeaderboard = async(req, res) => {
         res.status(500).send("Error occured when fetching the leaderboard");
     }
 };
+
+// filtering leaderboard
+module.exports.applyFilters = (filter) => {
+    let filterCriteria = {};
+
+    if(filter === "highSpenders"){
+        filterCriteria = {moneySpent: {$gte: 1500}} //users that have spent exactly or over $1500 are considered high spenders
+    }
+    else if(filter === "longestPlayers"){
+        filterCriteria = {timeSpent: {$gte: 100}} //users that have spent exactly or over 100hrs are considered the longest players
+    }
+
+    return filterCriteria;
+  };
+
+// calculating the win - loss ratio
+module.exports.calculateWinLossRatio = (users) => {
+    return users.map(user => ({
+        ...user,
+        winLossRatio: user.losses > 0 
+        ? (user.wins/user.losses).toFixed(2)
+        : user.wins
+    }));
+  };
+
+// sorting and ordering the leaderboard 
+module.exports.sortLeaderboard = (users, sortBy, order) => {
+    return users.sort((user1, user2) => {
+        if(sortBy === "username"){
+            return order === "asc" 
+            ? user1.username.toLowerCase().localeCompare(user2.username.toLowerCase()) 
+            : user2.username.toLowerCase().localeCompare(user1.username.toLowerCase());
+        }
+
+        return order === "asc" 
+        ? user1[sortBy] - user2[sortBy] 
+        : user2[sortBy] - user1[sortBy];
+    });
+  };
