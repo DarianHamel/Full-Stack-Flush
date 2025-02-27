76a667aa318915d83244920ed42006b32048e945
Chineze-prog <chinezeobi.bomi@gmail.com>
2025-02-27 12:53:34 -0600

    added tests for the client side of the leaderboard feature and updated the ui

--- client/src/__tests__/Leaderboard.test.js
+++ client/src/__tests__/Leaderboard.test.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Leaderboard from '../components/Leaderboard';
 
 describe('Leaderboard Component', () => {
-  test('renders Leaderboard component', () => {
+  test('renders sorting dropdown options', () => {
     render(
       <Router>
         <Leaderboard />
@@ -13,6 +13,64 @@ describe('Leaderboard Component', () => {
     );
 
     // Add assertions to verify the rendered output
+    expect(screen.getByText('Rank')).toBeInTheDocument();
+    expect(screen.getByText('Username')).toBeInTheDocument();
     expect(screen.getByText('Wins')).toBeInTheDocument();
+    expect(screen.getByText('Losses')).toBeInTheDocument();
+    expect(screen.getByText('Win/Loss Ratio')).toBeInTheDocument();
+    expect(screen.getByText('Money Spent')).toBeInTheDocument();
+    expect(screen.getByText('Time Spent')).toBeInTheDocument();
+  });
+
+  test('renders sorting dropdown options', () => {
+    render(
+      <Router>
+        <Leaderboard />
+      </Router>
+    );
+
+    // Add assertions to verify the rendered output
+    expect(screen.getByText('Descending Order')).toBeInTheDocument();
+    expect(screen.getByText('Ascending Order')).toBeInTheDocument();
+  });
+
+  test('renders sorting dropdown options', () => {
+    render(
+      <Router>
+        <Leaderboard />
+      </Router>
+    );
+
+    // Add assertions to verify the rendered output
+    expect(screen.getByText('All Users')).toBeInTheDocument();
+    expect(screen.getByText('High Spenders ($1500+)')).toBeInTheDocument();
+    expect(screen.getByText('Lomgest Platime (100+ hrs)')).toBeInTheDocument();
+  });
+
+  test('renders sorting dropdown options', () => {
+    render(
+      <Router>
+        <Leaderboard />
+      </Router>
+    );
+
+    // Add assertions to verify the rendered output
+    expect(screen.getByText('Rank')).toBeInTheDocument();
+    expect(screen.getByText('Username')).toBeInTheDocument();
+    expect(screen.getByText('Wins')).toBeInTheDocument();
+    expect(screen.getByText('Losses')).toBeInTheDocument();
+    expect(screen.getByText('Win/Loss Ratio')).toBeInTheDocument();
+    expect(screen.getByText('Money Spent')).toBeInTheDocument();
+    expect(screen.getByText('Time Spent')).toBeInTheDocument();
+  });
+
+  test('displays "No data available" when leaderboard is empty', () => {
+    render(
+      <Router>
+        <Leaderboard />
+      </Router>
+    );
+
+    expect(screen.getByText('No data available')).toBeInTheDocument();
   });
 });
