7d7fcbfc0cebbdf44066098ff78e8557093589e1
DarianHamel <dz.hamel@gmail.com>
2025-02-28 10:11:04 -0600

    Updated blackjack and profile to have requirements of being logged in

--- client/src/components/GamblingReminders.jsx
+++ client/src/components/GamblingReminders.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { toast } from "react-toastify";
 
+const remindTimer = 60000; // 60 seconds
+
 const GamblingReminders = () => {
   useEffect(() => {
     const gamblingFacts = [
@@ -16,7 +18,7 @@ const GamblingReminders = () => {
       toast.info(randomFact, { position: "top-center" });
     };
 
-    const intervalId = setInterval(showGamblingFact, 60000); // Show a fact every 60 seconds
+    const intervalId = setInterval(showGamblingFact, remindTimer); // Show a fact every 60 seconds
 
     return () => clearInterval(intervalId);
   }, []);
